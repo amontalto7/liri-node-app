67e6cb6e27aaad3de85edd535c04826e8491d922
ant4u <anthonyimontalto@gmail.com>
2018-12-04 18:01:38 -0500

    fixed movie query

--- liri.js
+++ liri.js
@@ -19,27 +19,37 @@ function getSong() {
 }
 
 function getMovie(p) {
-    console.log("get movie");
     var movieName = p.join("+");
     // Then run a request with axios to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
+    function getRottenTomatoesScore(r){
+        for (var i = 0; i < r.length; i++) {
+            if (r[i].Source === "Rotten Tomatoes") {
+                return r[i].Value;
+                break;
+            }
+        }
+    }
     
     axios.get(queryUrl).then(
         function(response){
-        console.log(response);
+        // console.log(response);
         // If the request with axios is successful
         // ...
 
             if (response.status === 200){
             // Then log the Release Year for the movie
             var cast = response.data.Actors.split(",")
+            var rtRating = getRottenTomatoesScore(response.data.Ratings);
+
             console.log("Title: " + response.data.Title);
             console.log("Year: " + response.data.Year);
             console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: ??????" + JSON.stringify(response.data.Ratings));
+            console.log("Rotten Tomatoes Rating: " + rtRating);
+            // JSON.stringify(response.data.Ratings,undefined,2));
             console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.English);
+            console.log("Language(s): " + response.data.Language);
             console.log("\nPlot: \n" + response.data.Plot);
             console.log("\nCast:");
             cast.forEach(element => {
@@ -50,8 +60,7 @@ function getMovie(p) {
     )
 
 // This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
+// console.log(queryUrl);
 
 }
 
