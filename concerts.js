bd3a32e39ad545ba81b33839779053ec2bba9032
ant4u <anthonyimontalto@gmail.com>
2018-12-06 22:30:34 -0500

    concert data complete

--- concerts.js
+++ concerts.js
@@ -1,25 +1,26 @@
 var axios = require("axios");
+var moment = require('moment');
 
 module.exports = {
   getConcert: function(p) {
-    console.log("get concert");
       var artist = p.join("%20");
     var queryUrl =
       "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-    // "https://rest.bandsintown.com/artists/" + artist + "events?app_id=codingbootcamp";
-
     axios
       .get(queryUrl)
       .then(function(response) {
-        console.log(response);
-      })
+        // console.log(response);
+        console.log("\n"+p.join(" ") +" is performing at...");
+        for (var i = 0; i < response.data.length; i++) {
+        console.log("\n" + response.data[i].venue.name);
+        console.log(response.data[i].venue.city + ", " + response.data[i].venue.region)
+        console.log(moment(response.data[i].datetime).format("MMMM DD, YYYY"));
+    }
+    })
       .catch(function(error) {
         console.log(error);
       });
 
-    console.log(queryUrl);
   }
-
-  // queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp
 };
